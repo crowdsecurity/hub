798ea0d8bab087fd786b952376978572e2eb75f7
erenJag <erenJag>
2020-12-09 14:58:14 +0100

    add test generation

--- tests.sh
+++ tests.sh
@@ -4,10 +4,11 @@ usage() {
       echo "Usage:"
       echo "    ./tests.sh -h|--help                        Display this help message."
       echo "    ./tests.sh -i                               Init tests : prepare env tests"
+      echo "    ./tests.sh -g <CONFIG_PATH/name.yaml>       Generate new test by specifying target config (parser|scenario|postoverflow)"
       echo "    ./tests.sh --all                            Run all tests"
-      echo "    ./tests.sh --single                         Run single test : ./tests.sh --single <MYPATH/config.yaml>"
+      echo "    ./tests.sh --single <MYPATH/config.yaml>    Run single test"
 
-      exit 0  
+      exit 0
 }
 
 init_tests() {
@@ -15,6 +16,30 @@ init_tests() {
     cd hub-tests/ && go build && cd ..
     cp -r hub-tests/config/ .
     mkdir config/hub data && cp .index.json config/hub/
+}
+
+generate_config() {
+    if [[ -n  $1 ]];
+    then
+	TEST=$1
+	ITEM_TYPE=$(echo $TEST | awk -F "/" '{print $1}')
+	ITEM_NAME=$(echo $TEST | awk -F "/" '{print $(NF-1)"/"$(NF)}')
+	ITEM_NAME=$(echo $ITEM_NAME | awk -F "." '{print $1}')
+	mkdir -p $(dirname $TEST)/.tests/$(basename $TEST .yaml)
+	cat <<EOF > $(dirname $TEST)/.tests/$(basename $TEST .yaml)/config.yaml
+log_file:  test.log #unused for now, will need rework when acquis.yaml will part of parsers
+parser_results: parser_results.json
+bucket_input: bucket_input.yaml
+bucket_results: bucket_result.json
+postoverflow_input: postoverflow_input.yaml
+postoverflow_results: postoverflow_results.json
+#configuration
+index: "./config/hub/.index.json"
+configurations:
+  $ITEM_TYPE:
+  - $ITEM_NAME
+EOF
+    fi
 }
 
 run_all_tests() {
@@ -35,6 +60,10 @@ do
     case ${key} in
     -i)
         init_tests
+        exit 0
+        ;;
+    -g)
+        generate_config ${2}
         exit 0
         ;;
     --all)
