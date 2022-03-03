a9cd5eb94e10e704da78034b40a4e08879221e43
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2022-03-03 18:15:07 +0100

    fix emby collections and index generation script (#402)
    * fix emby collections and index generation script

--- generate.go
+++ generate.go
@@ -27,6 +27,7 @@ func (ti *typeInfo) generate(filepath string, configType string) (string, error)
 	pathSplit := strings.Split(filepath, "/")
 	//generate doc path ?
 	pdocpath := strings.Replace(filepath, ".yaml", ".md", 1)
+	pdocpath = strings.Replace(pdocpath, ".yml", ".md", 1)
 
 	if pathSplit[0] != configType {
 		return "", fmt.Errorf("invalid filepath (doesn't start with scenarios) : %s", filepath)
@@ -175,7 +176,7 @@ func generateIndex(configType string) (map[string]typeInfo, error) {
 	folder := path.Join("./", configType)
 
 	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
-		if strings.HasSuffix(path, ".yaml") {
+		if strings.HasSuffix(path, ".yaml") || strings.HasSuffix(path, ".yml") {
 			files = append(files, path)
 		}
 		return nil
