98fa4b90bc539cc52cbb50ffc4cf04cb46083a0e
alteredCoder <kevin@crowdsec.net>
2023-05-30 17:59:38 +0200

    add doc generation script

---
+++ scripts/generate_doc.py
@@ -0,0 +1,75 @@
+import os
+import json
+import argparse
+
+from mdutils.mdutils import MdUtils
+
+BEHAVIORS_DOC_FILE = "behaviors.md"
+SCENARIOS_DOC_FILE = "scenarios.md"
+
+
+def generate_behaviors_doc(filepath, data):
+    md = MdUtils(file_name=filepath, title="Behaviors Taxonomy")
+    headers = ["Name", "Label", "Description"]
+    rows = []
+    for behavior, info in data.items():
+        rows.extend([info["name"], info["label"], info["description"]])
+
+    md.new_line()
+    md.new_table(columns=len(headers), rows=len(data), text=rows, text_align='center')
+
+    md.create_md_file()
+
+
+def generate_scenarios_doc(filepath, data):
+    md = MdUtils(file_name=filepath, title="Scenarios Taxonomy")
+    headers = ["Name", "Label", "Description", "Behaviors", "Mitre ATT&CK", "CVES", "Spoofable", "Confidence"]
+    rows = []
+    for scenarios, info in data.items():
+        rows.extend([
+            info["name"],
+            info["label"],
+            info["description"],
+            info["behaviors"],
+            info["mitre_attacks"],
+            info["cves"] if "cves" in info else "",
+            info["spoofable"],
+            info["confidence"],
+        ])
+
+    md.new_line()
+    md.new_table(columns=len(headers), rows=len(data), text=rows, text_align='center')
+
+    md.create_md_file()
+
+
+
+def main():
+    args = parse_args()
+    mitre_data = json.load(open(args.mitre, "r"))
+    behavior_data = json.load(open(args.behaviors, "r"))
+    scenario_data = json.load(open(args.scenarios, "r"))
+    
+    behaviors_doc_filepath = os.path.join(args.output, BEHAVIORS_DOC_FILE)
+    scenarios_doc_filepath = os.path.join(args.output, SCENARIOS_DOC_FILE)
+
+    generate_behaviors_doc(behaviors_doc_filepath, behavior_data)
+    generate_scenarios_doc(scenarios_doc_filepath, scenario_data)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate CrowdSec Scenarios taxonomy file')
+
+    parser.add_argument('--hub', type=str, help="Hub folder path", default="")
+    parser.add_argument('-o', '--output', type=str, help="Output folder", default="./")
+    parser.add_argument('-b', '--behaviors', type=str, help="behaviors.json filepath", default="./behaviors.json")
+    parser.add_argument('-m', '--mitre', type=str, help="mitre_attack.json filepath", default="./mitre_attack.json")
+    parser.add_argument('-s', '--scenarios', type=str, help="scenario.json filepath", default="./scenarios.json")
+    parser.add_argument('-v', '--verbose', action="store_true", help="Verbose mode", default=False)
+    return parser.parse_args()
+
+
+
+
+if __name__ == "__main__":
+    main()
